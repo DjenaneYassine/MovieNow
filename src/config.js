150e0d01d3155bb54a78e61dadb36363af45f440
Djenane Yassine <djenane.yassine1@gmail.com>
2024-04-12 01:29:04 +0200

    reactshow

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY_PARAM = "?api_key=733bab7e203ec9e55b393a478e011e14";
+const API_KEY_PARAM = "?api_key="; //add your API KEY
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
 const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300/";
 export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL };
